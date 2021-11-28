552f75a14e3fe70811906a5c0f91b1a28aa18912
Alfindo <75003444+alfinkresna@users.noreply.github.com>
2021-11-28 16:07:17 +0700

    Add files via upload

---
+++ emailvalid.php
@@ -0,0 +1,29 @@
+<?php
+
+    $curl = curl_init();
+
+    $data = [
+    echo    'email' => "email",
+    ];
+
+    $post_data = http_build_query($data);
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://www.disify.com/api/email",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $post_data,
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo $response;
+    }
+
+?>
